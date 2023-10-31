e0078bc1e3d382eb7e8a8a953911e87ad67380da
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-10-31 06:53:56 -0400

    Create game.js
    Create game.js; handles the code and initiation of the Phaser framework.

---
+++ game/game.js
@@ -0,0 +1,41 @@
+//Phaser.js Config goes here
+
+//A global object to pass values between functions and files alike
+const gameState = {
+  nextScene: false, //Scene flag for changing to the next scene
+  escape: false, //Is set to true in some scene files when the player presses esc
+};
+
+//Config creates the entire scene, you can change backgroundColor and game size here without breaking any other code
+const config = {
+  height: 225,
+  width: 400,
+  backgroundColor: 0x212121, //A Very Dark Grey
+  scene: [Title, Scene1],
+  pixelArt: true, //Turns off anti-aliasing
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 15 },
+      debug: false, //Set to true to turn on debug mode (shows hitboxes and velocities)
+    }
+  },
+  scale: {
+    parent: document.body.querySelector(".game"),
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      height: 225,
+      width: 400,
+    },
+    max: {
+      height: 9000,
+      width: 16000,
+    },
+    zoom: 1,
+  },
+  autoRound: false,
+};
+
+//Initiates the config
+const game = new Phaser.Game(config);
