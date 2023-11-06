5f01dc586ad3d6aec9feb63293fdb2d67286d4ea
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-11-05 19:50:41 -0500

    Update scene1.js
    Almost completed the choose your character screen

--- game/scene1.js
+++ game/scene1.js
@@ -1,13 +1,13 @@
 /*
 Task List:
 [x] Set characters to interactive
-  [ ] On hover: enlarge
+  [x] On hover: enlarge
   [ ] On click: give description of character and allow player to confirm their choice
     [ ] On confirm: set gameState.character to a 1 (dragon) or a 2 (snake), then set gameState.nextLevel to true
-[ ] Add next scene code
-[ ] Add title "Choose Your Character"
-[ ] Add map background
-[ ] Add character text under each (e.g. "Dragon" or "Snake")
+[x] Add next scene code
+[x] Add title "Choose Your Character"
+[x] Add map background
+[x] Add character text under each (e.g. "Dragon" or "Snake")
 */
 class Scene1 extends Phaser.Scene {
   constructor() {
@@ -15,12 +15,31 @@ class Scene1 extends Phaser.Scene {
   }
   preload() {
     this.load.spritesheet("sheet1", "assets/sprites/32x32sprites1.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.image("mapBG", "assets/sprites/MapBG.png");
   }
   create() {
+    //Create map background image
+    this.add.sprite(0, 0, "mapBG").setDepth(0);
+    
     //Create character sprites and style them so the player can choose
     gameState.dragon = this.add.sprite(144, 112, "sheet1").setDepth(2).setScale(2).setFrame(6).setInteractive();
     gameState.snake = this.add.sprite(256, 112, "sheet1").setDepth(2).setScale(2).setFrame(21).setInteractive();
 
+    //Create and style the title
+    let titleText = this.add.text(200, 30, "Choose Your Character", { fontFamily: "FiveBySeven", fontSize: "50px", fill: "#f4f4f4", align: "center", lineSpacing: -25 });
+    titleText.setOrigin(titleText.halfWidth, 0);
+    titleText.setShadow(-6, 6, "#454545", 0);
+    titleText.setDepth(1);
+
+    //Create & style the class name texts
+    let dragonText = this.add.text(144, 144, "Dragon", { fontFamily: "FiveBySeven", fontSize: "20px", fill: "#f4f4f4" });
+    dragonText.setOrigin(dragonText.halfWidth, 0);
+    dragonText.setDepth(1);
+    
+    let snakeText = this.add.text(256, 144, "Snake", { fontFamily: "FiveBySeven", fontSize: "20px", fill: "#f4f4f4" });
+    snakeText.setOrigin(snakeText.halfWidth, 0);
+    snakeText.setDepth(1);
+    
     //Create player animations & torch animation
     this.anims.create({
       key: 'dragonIdle',
@@ -35,6 +54,36 @@ class Scene1 extends Phaser.Scene {
       frameRate: 10,
       repeat: 1
     });
+
+    //On hover: change color and scale
+    gameState.dragon.on('pointerover', () => {
+      gameState.dragon.setScale(2.2);
+    });
+    
+    //No hover: reset color and scale
+    gameState.dragon.on('pointerout', () => {
+      gameState.dragon.setScale(2);
+    });
+    
+    //On click: start the game
+    //gameState.dragon.on('pointerup', () => {
+    //  gameState.nextLevel = true;
+    //});
+
+    //On hover: change color and scale
+    gameState.snake.on('pointerover', () => {
+      gameState.snake.setScale(2.2);
+    });
+    
+    //No hover: reset color and scale
+    gameState.snake.on('pointerout', () => {
+      gameState.snake.setScale(2);
+    });
+    
+    //On click: start the game
+    //gameState.snake.on('pointerup', () => {
+    //  gameState.nextLevel = true;
+    //});
 
     //If ESC is pressed, go back to the title
     this.input.keyboard.on('keydown-ESC', function () {gameState.escape = true});
@@ -56,6 +105,12 @@ class Scene1 extends Phaser.Scene {
       gameState.snakeCounter = 0;
     };
 
+    //If a character is chosen, go to next scene
+    if (gameState.nextLevel) {
+      this.scene.stop("Scene1");
+      this.scene.start("Scene2");
+    };
+    
     //If ESC is pressed, go back to the title
     if (gameState.escape) {
       gameState.escape = false;
