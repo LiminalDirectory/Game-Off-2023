c143b5405017fe4a3df989f739eda889b27c5514
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-11-06 07:46:47 -0500

    Create scene2.js
    Set up a basic skeleton for the scene and created a task list

---
+++ game/scene2.js
@@ -0,0 +1,58 @@
+/*
+Task List
+[ ] Show the upcoming battles, shops, map stuff, etc.
+  [ ] Draw lines between the ones you can and can't go to
+[ ] Animation for returning to the map
+[ ] Enlarge location on hover
+[ ] Destroy lines to non-chosen locations on click
+[ ] Add map key
+*/
+class Scene2 extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Scene2' });
+  }
+  preload() {
+    this.load.spritesheet("sheet1", "assets/sprites/32x32sprites1.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.image("mapBG", "assets/sprites/MapBG.png");
+  }
+  create() {
+    //Create map background image
+    this.add.sprite(200, 112, "mapBG").setDepth(0);
+
+    //On hover: change scale
+    gameState.dragon.on('pointerover', () => {
+      gameState.dragon.setScale(2.2);
+    });
+
+    //No hover: reset scale
+    gameState.dragon.on('pointerout', () => {
+      gameState.dragon.setScale(2);
+    });
+
+    //On click: start the game
+    gameState.dragon.on('pointerup', () => {
+      gameState.character = 1;
+      gameState.nextScene = true;
+    });
+
+    //If ESC is pressed, go back to the title
+    this.input.keyboard.on('keydown-ESC', function () {gameState.escape = true});
+  }
+  update() {
+    //If a condition is met, go to next scene
+    if (gameState.nextScene) {
+      this.scene.stop("Scene2");
+      this.scene.start("Scene3");
+    };
+
+    //If ESC is pressed, go back to the title
+    if (gameState.escape) {
+      gameState.escape = false;
+      //Stop playing the game's music
+      //document.querySelector(".music").load();
+      //document.querySelector(".music").pause();
+      this.scene.stop("Scene2");
+      this.scene.start("Title");
+    };
+  };
+};
