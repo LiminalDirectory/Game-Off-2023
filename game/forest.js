d0b1a644ccae88942148bbb0d1dc40bfe2a84532
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-11-08 13:54:52 -0500

    Update forest.js
    Added code to render the map locations

--- game/forest.js
+++ game/forest.js
@@ -1,6 +1,6 @@
 /*
 Task List
-[ ] Show the upcoming battles, shops, map stuff, etc.
+[x] Show the upcoming battles, shops, map stuff, etc.
   [ ] Draw lines between the ones you can and can't go to
 [ ] Animation for returning to the map
 [ ] Enlarge location on hover
@@ -18,6 +18,74 @@ class Forest extends Phaser.Scene {
   create() {
     //Create map background image
     this.add.sprite(200, 112, "keyMapBG").setDepth(0);
+
+    //Create static groups for each event location
+    let battleGroup = this.physics.add.staticGroup();
+    let eliteGroup = this.physics.add.staticGroup();
+    let traderGroup = this.physics.add.staticGroup();
+    let relicGroup = this.physics.add.staticGroup();
+    let randomGroup = this.physics.add.staticGroup();
+    let shopGroup = this.physics.add.staticGroup();
+
+    if (gameState.location === -1) {
+      for (let i = 1; i < 4; i++) {
+        let yDistance = (225 - gameState.mapF[location + i].length * 32) / (gameState.mapF[location + i].length + 1);
+        gameState.mapF[location + i].forEach(function (value, index) {
+          let event;
+          if (value === 0) {event = battleGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(0); continue};
+          if (value === 1) {event = eliteGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(1); continue};
+          if (value === 3) {event = relicGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(3); continue};
+          if (value === 4) {event = randomGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(4); continue};
+          if (value === 5) {event = shopGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(5); continue};
+          event.setInteractive();
+          //On hover: change color and scale
+          event.on('pointerover', () => {
+            event.setScale(1.1);
+          });
+    
+          //No hover: reset color and scale
+          event.on('pointerout', () => {
+            event.setScale(1);
+          });
+          
+          //On click: start the game
+          event.on('pointerup', () => {
+            gameState.nextScene = value;
+            gameState.currentEvent = value;
+          });
+        });
+      };
+    } else {
+      this.add.sprite(75, 112, "sheet1").setFrame(gameState.currentEvent);
+      for (let i = 2; i < 4; i++) {
+        let yDistance = (225 - gameState.mapF[location + i].length * 32) / (gameState.mapF[location + i].length + 1);
+        gameState.mapF[location + i].forEach(function (value, index) {
+          let event;
+          if (value === 0) {event = battleGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(0); continue};
+          if (value === 1) {event = eliteGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(1); continue};
+          if (value === 2) {event = traderGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(2); continue};
+          if (value === 3) {event = relicGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(3); continue};
+          if (value === 4) {event = randomGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(4); continue};
+          if (value === 5) {event = shopGroup.create(25 + i * 50, yDistance + 2 * yDistance * index, "sheet1").setFrame(5); continue};
+          event.setInteractive();
+          //On hover: change color and scale
+          event.on('pointerover', () => {
+            event.setScale(1.1);
+          });
+    
+          //No hover: reset color and scale
+          event.on('pointerout', () => {
+            event.setScale(1);
+          });
+          
+          //On click: start the game
+          event.on('pointerup', () => {
+            gameState.nextScene = value;
+            gameState.currentEvent = value;
+          });
+        });
+      };
+    };
 
     //On hover: change scale
     gameState.dragon.on('pointerover', () => {
@@ -40,10 +108,10 @@ class Forest extends Phaser.Scene {
   }
   update() {
     //If a condition is met, go to next scene
-    //if (gameState.nextScene) {
-    //  this.scene.stop("Forest");
-    //  this.scene.start("Shop");
-    //};
+    if (gameState.nextScene === 0) {
+      this.scene.stop("Forest");
+      this.scene.start("BattleF");
+    };
 
     //If ESC is pressed, go back to the title
     if (gameState.escape) {
