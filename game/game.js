63117a38a0261233779fbb266cdd835153fa44e6
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-11-08 18:19:31 -0500

    Update game.js
    Updated the use of the makeMap function to fit its new code

--- game/game.js
+++ game/game.js
@@ -13,11 +13,11 @@ const gameState = {
   mapV: 0, //Will eventually hold the 2d array for the volcano map
 };
 
-makeMap(gameState.mapF);
-makeMap(gameState.mapC);
-makeMap(gameState.mapT);
-makeMap(gameState.mapM);
-makeMap(gameState.mapV);
+gameState.mapF = makeMap();
+gameState.mapC = makeMap();
+gameState.mapT = makeMap();
+gameState.mapM = makeMap();
+gameState.mapV = makeMap();
 
 //Config creates the entire scene, you can change backgroundColor and game size here without breaking any other code
 const config = {
