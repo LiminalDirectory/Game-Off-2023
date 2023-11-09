c22635b03fc3ba335c767732997df522ae000ee5
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2023-11-08 20:33:38 -0500

    Update game.js
    Made a few new variables

--- game/game.js
+++ game/game.js
@@ -7,17 +7,23 @@ const gameState = {
   currentEvent: -1, //What event the player is at (battle, elite, treasure, shop, etc.)
   location: -1, //Current location on the map
   mapF: 0, //Will eventually hold the 2d array for the forest map
+  mapLinesF: 0, //Will eventually hold the 2d array for the forest map's lines
   mapC: 0, //Will eventually hold the 2d array for the cave map
+  mapLinesC: 0, //Will eventually hold the 2d array for the forest map's lines
   mapT: 0, //Will eventually hold the 2d array for the taiga map
+  mapLinesT: 0, //Will eventually hold the 2d array for the forest map's lines
   mapM: 0, //Will eventually hold the 2d array for the mountain map
+  mapLinesM: 0, //Will eventually hold the 2d array for the forest map's lines
   mapV: 0, //Will eventually hold the 2d array for the volcano map
+  mapLinesV: 0, //Will eventually hold the 2d array for the forest map's lines
 };
 
-gameState.mapF = makeMap();
-gameState.mapC = makeMap();
-gameState.mapT = makeMap();
-gameState.mapM = makeMap();
-gameState.mapV = makeMap();
+gameState.mapF = makeMap(0);
+gameState.mapC = makeMap(1);
+gameState.mapT = makeMap(2);
+gameState.mapM = makeMap(3);
+gameState.mapV = makeMap(4);
+
 
 //Config creates the entire scene, you can change backgroundColor and game size here without breaking any other code
 const config = {
